388c19af4156e43c0a0ab431bd70c8e557e80984
jjeonmin <jms959310@gmail.com>
2024-08-23 23:20:32 +0900

    feat: 서비스 추가

--- src/main/java/com/nhnacademy/auth/controller/JoinController.java
+++ src/main/java/com/nhnacademy/auth/controller/JoinController.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.auth.controller;
 
+import com.nhnacademy.auth.dto.JoinDto;
+import com.nhnacademy.auth.service.JoinService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/nhnacademy/auth/service/JoinService.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.auth.service;
+
+import com.nhnacademy.auth.dto.JoinDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+    public void joinProcess(JoinDto joinDto){
+
+    }
+}
