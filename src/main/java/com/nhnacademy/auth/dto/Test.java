9c9601e220afd205e135c2c0780e04ec9b801eb8
jjeonmin <jms959310@gmail.com>
2024-09-07 14:24:33 +0900

    feat: 추가

--- src/main/java/com/nhnacademy/auth/dto/Test.java
+++ src/main/java/com/nhnacademy/auth/dto/Test.java
@@ -2,9 +2,12 @@ package com.nhnacademy.auth.dto;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Test {
     @Id
     int student_num;
+    String student_name;
 }
