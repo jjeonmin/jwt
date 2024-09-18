020ec777d73f6bf94d93b0b9cca320adc9413fcb
jjeonmin <jms959310@gmail.com>
2024-09-18 23:30:30 +0900

    feat: 추가

--- src/main/java/com/nhnacademy/auth/service/JoinService.java
+++ src/main/java/com/nhnacademy/auth/service/JoinService.java
@@ -1,9 +1,11 @@
 package com.nhnacademy.auth.service;
 
 import com.nhnacademy.auth.dto.JoinDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class JoinService {
     public int joinProcess(JoinDto joinDto){
         int a= joinDto.getTest();
