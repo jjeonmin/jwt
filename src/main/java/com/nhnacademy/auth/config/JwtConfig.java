2a01fda44422432ba6025bc3c5f690bdce691dd9
jjeonmin <jms959310@gmail.com>
2024-09-28 23:51:39 +0900

    feat: 추가

--- src/main/java/com/nhnacademy/auth/config/JwtConfig.java
+++ src/main/java/com/nhnacademy/auth/config/JwtConfig.java
@@ -1,9 +1,11 @@
 package com.nhnacademy.auth.config;
 
+import com.nhnacademy.auth.dto.Test;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JwtConfig {
+    Test test;
 
 
 }
