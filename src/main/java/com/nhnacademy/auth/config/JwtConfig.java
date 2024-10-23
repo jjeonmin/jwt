d568c8e002e2db9ac19cf96055f8f645ef08c20e
jjeonmin <jms959310@gmail.com>
2024-10-23 23:54:06 +0900

    feat: 추가

--- src/main/java/com/nhnacademy/auth/config/JwtConfig.java
+++ src/main/java/com/nhnacademy/auth/config/JwtConfig.java
@@ -9,7 +9,7 @@ public class JwtConfig {
 
     public Test getTest(String a, int c) {
         boolean b = a.equals(test);
-        if (b==false && a.equals("a")){
+        if (b==false && a.equals("b")){
             return test;
         }
         else {
